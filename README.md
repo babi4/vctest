7871931c8b082b630c7fe62a381ddcfb909de299
babi4 <babichevv@gmail.com>
2016-04-15 12:12:05 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# vc test
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
+
+```sh
+$ git clone git@github.com:babi4/vctest.git # or clone your own fork
+$ cd vctest
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+or
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
---
+++ index.js
@@ -0,0 +1,125 @@
+'use strict';
+//thanks @voronianski and https://github.com/voronianski/simon-le-bottle
+
+
+// Getting started with Facebook Messaging Platform
+// https://developers.facebook.com/docs/messenger-platform/quickstart
+
+var express = require('express');
+var request = require('superagent');
+var bodyParser = require('body-parser');
+
+var pageToken = 'CAAJALMpLGkQBAGjETZCnU7pnZCfx2ZCEY2s4eV26eW8EtZANFjpCsAbF38ehZAtfkbkOOWOr6PpML2Hsq0gBsQynv2WS8PIPT9pIJZCYF7n7g9AZBmbeMydViR611hMfnfATtaLXzhdUNDKim7HW2IxRp5Dk6ZC09S2Pz8TeLfznE8TZBz83qHXZC68mZANN0pMngbh18csgEU0agZDZD';
+var verifyToken = 'HELLO';
+
+var app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.verify_token'] === verifyToken) {
+        return res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
+});
+
+app.post('/webhook', function(req, res) {
+    var messagingEvents = req.body.entry[0].messaging;
+
+    messagingEvents.forEach(function(event) {
+        var sender = event.sender.id;
+
+        if (event.postback) {
+            var text = JSON.stringify(event.postback).substring(0, 200);
+            sendTextMessage(sender, 'Postback received: ' + text);
+        } else if (event.message && event.message.text) {
+            var text = event.message.text.trim().substring(0, 200);
+
+            if (text.toLowerCase() === 'generic') {
+                sendGenericMessage(sender);
+            } else {
+                sendTextMessage(sender, 'Text received, echo: ' + text);
+            }
+        }
+    });
+
+    res.sendStatus(200);
+});
+
+function sendMessage (sender, message) {
+    request
+        .post('https://graph.facebook.com/v2.6/me/messages')
+        .query({access_token: pageToken})
+        .send({
+            recipient: {
+                id: sender
+            },
+            message: message
+        })
+        .end(function (err, res) {
+            if (err) {
+                console.log('Error sending message: ', err);
+            } else if (res.body.error) {
+                console.log('Error: ', res.body.error);
+            }
+        });
+}
+
+function sendTextMessage (sender, text) {
+    sendMessage(sender, {
+        text: text
+    });
+}
+
+function sendGenericMessage (sender) {
+    sendMessage(sender, {
+        attachment: {
+            type: 'template',
+            payload: {
+                template_type: 'generic',
+                elements: [{
+                    title: 'First card',
+                    subtitle: 'Element #1 of an hscroll',
+                    image_url: 'http://messengerdemo.parseapp.com/img/rift.png',
+                    buttons: [{
+                        type: 'web_url',
+                        url: 'https://www.messenger.com/',
+                        title: 'Web url'
+                    }, {
+                        type: 'postback',
+                        title: 'Postback',
+                        payload: 'Payload for first element in a generic bubble'
+                    }]
+                }, {
+                    title: 'Second card',
+                    subtitle: 'Element #2 of an hscroll',
+                    image_url: 'http://messengerdemo.parseapp.com/img/gearvr.png',
+                    buttons: [{
+                        type: 'postback',
+                        title: 'Postback',
+                        payload: 'Payload for second element in a generic bubble'
+                    }]
+                }]
+            }
+        }
+    });
+}
+
